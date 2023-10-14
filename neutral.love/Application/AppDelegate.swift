760e3a01bc26ff97617bf039e99fef4539c81486
philippzeppelin <philippzeppelin@gmail.com>
2023-10-14 15:44:01 +0300

    Remove storyboard

---
+++ neutral.love/Application/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  neutral.love
+//
+//  Created by Philipp Zeppelin on 05.10.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
