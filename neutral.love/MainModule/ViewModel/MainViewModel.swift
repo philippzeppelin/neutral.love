9066bf02f2eb0f66dcd9381ee2164278b6a5cc03
Sergei Smirnov <84528261+SerggSmirnov@users.noreply.github.com>
2023-11-10 20:39:07 +0300

    Squashed commit of the following: (#8)
    commit b74052241e5b2e70b5035635cbc334ce84a5bd79
    Merge: ded9064 71a6b4d
    Author: philippzeppelin <104386097+philippzeppelin@users.noreply.github.com>
    Date:   Mon Nov 6 21:17:43 2023 +0300
    
        Merge pull request #6 from philippzeppelin/NL-13-Create-SignUp_Module-UI
    
        NL-13 Create Sign Up Module UI
    
    commit 71a6b4d8d2de2fc7bb06774e46370339ceec1345
    Author: philippzeppelin <philippzeppelin@gmail.com>
    Date:   Sun Oct 29 12:51:54 2023 +0300
    
        Added UI elements and ScrollView
    
        add collection view and page control
    
        finish main module UI
    
        resolve commits
    
        fix & gitignore
    
        NL-6-Create-generate-module (#4)
    
        fix review 3
    
        fix review 2
    
        fix review 1
    
        finish UI
    
        add Fonts
    
        NL-2 create sign in module (#5)
    
        * Get ready project for adding AuthModule
    
        * Save for the day 15.10
    
        * Added AuthSegmentedControll, cells for Email and Password
    
        * Added View and Controller for SignInModule
    
        * Added Email and Password TextFields, labels and SignIn/SignUp buttons
    
        * Starting set all magic numbers, texts and fonts into Resoursec
    
        * Finished UI and added Assembly
    
        * Added scrollView to SignInView
    
        * Finished UI SignIn Module
    
        * Resolve PR comments 1.0
    
        * Resolve PR comments 1.1
    
        * Commit for PR
    
        * Update SignUpViewController.swift
    
        * Squashed commit of the following
    
        Resolve comments
    
        Some fixes
    
    commit ea40629c4ee56836db0cfc46ddfac3e5d207e622
    Author: philippzeppelin <philippzeppelin@gmail.com>
    Date:   Fri Oct 27 15:43:55 2023 +0300
    
        NL-13-Create-SignUp-Module-UI
    
        Added starting files and folders
    
        Added TextFields and background
    
        Save for the day 27.10
    
        Save
    
        Save
    
        Added UI elements and scrollView
    
    commit ded906409d29fd5d0bf93776379ea218171a710d
    Author: philippzeppelin <104386097+philippzeppelin@users.noreply.github.com>
    Date:   Sat Oct 28 18:28:30 2023 +0300
    
        NL-2 create sign in module (#5)
    
        * Get ready project for adding AuthModule
    
        * Save for the day 15.10
    
        * Added AuthSegmentedControll, cells for Email and Password
    
        * Added View and Controller for SignInModule
    
        * Added Email and Password TextFields, labels and SignIn/SignUp buttons
    
        * Starting set all magic numbers, texts and fonts into Resoursec
    
        * Finished UI and added Assembly
    
        * Added scrollView to SignInView
    
        * Finished UI SignIn Module
    
        * Resolve PR comments 1.0
    
        * Resolve PR comments 1.1
    
        * Commit for PR
    
        * Update SignUpViewController.swift
    
        * Squashed commit of the following
    
    fix3
    
    review fixs
    
    finish mok networklayer
    
    finish mok networklayer
    
    problem with async/await
    
    minimum working version
    
    review fix2

--- neutral.love/MainModule/ViewModel/MainViewModel.swift
+++ neutral.love/MainModule/ViewModel/MainViewModel.swift
@@ -7,6 +7,38 @@
 
 import Foundation
 
-protocol MainViewModelProtocol {}
+protocol MainViewModelProtocol {
+    var delegate: MainViewModelProtocolDelegate? { get set }
+    
+    var style: [String] { get }
+    var layout: [String] { get }
+    var amount: [String] { get }
+    
+    var outputs: [Output] { get set }
+    func fetchData()
+}
 
-final class MainViewModel: MainViewModelProtocol {}
+protocol MainViewModelProtocolDelegate: AnyObject {
+    func didLoadData()
+}
+
+final class MainViewModel: MainViewModelProtocol {
+    
+    weak var delegate: MainViewModelProtocolDelegate?
+    
+    var style = ["Photo", "Fantasy", "Anime", "Painting", "Sci-Fi", "Cyberpunk", "Pixelart", "Steampunk", "Synthwave"]
+    var layout = ["Square", "Vertical", "Horizontal"]
+    var amount = ["4", "8", "12"]
+    
+    var outputs = [Output]()
+    let apiManager = APIManager()
+    
+    func fetchData() {
+        Task {
+            let orderID = try await apiManager.fetchOrderID()
+            outputs = try await apiManager.fetchOrderInfoOutput(orderID: orderID)
+            
+            delegate?.didLoadData()
+        }
+    }
+}
