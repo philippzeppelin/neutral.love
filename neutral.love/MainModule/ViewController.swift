6ffdc54503dc3bc33289a0d7f32542497679ad59
philippzeppelin <philippzeppelin@gmail.com>
2023-11-15 22:17:48 +0300

    Fix errors after PR

--- neutral.love/MainModule/ViewController.swift
+++ neutral.love/MainModule/ViewController.swift
@@ -7,4 +7,10 @@
 
 import UIKit
 
-final class ViewController: UIViewController {}
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
